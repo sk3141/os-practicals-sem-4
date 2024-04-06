961a6be58c68f76c324945614bc952b7836d120c
sahil <sahil21052003@gmail.com>
2024-04-06 07:37:11 +0530

    06/04/24

--- banker_algo.c
+++ banker_algo.c
@@ -13,11 +13,26 @@ void initialize(int processNo, int resourceNo)
   available = (int**)malloc(processNo * sizeof(int*));
   need = (int**)malloc(processNo * sizeof(int*));
 
-  for (int i = 0; i < resourceNo; i++)
+  for (int i = 0; i < processNo; i++)
   {
     allocation[i] = (int*)malloc(resourceNo * sizeof(int));
     max[i] = (int*)malloc(resourceNo * sizeof(int));
     available[i] = (int*)malloc(resourceNo * sizeof(int));
     need[i] = (int*)malloc(resourceNo * sizeof(int));
   }
+}
+
+void deallocate(int processNo, int resourceNo)
+{
+  for (int i = 0; i < processNo; i++)
+  {
+    free(allocation[i]);
+    free(available[i]);
+    free(max[i]);
+    free(need[i]);
+  }
+  free(allocation);
+  free(available);
+  free(max);
+  free(need);
 }
