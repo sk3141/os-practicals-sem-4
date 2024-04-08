6cf39f2b50b5e443b2470eb7912f5cdf088a0016
sahil <sahil21052003@gmail.com>
2024-04-08 18:45:18 +0530

    08/04/24

--- page_replacement/lfu.c
+++ page_replacement/lfu.c
@@ -2,18 +2,17 @@
 #include <stdlib.h>
 #include <limits.h>
 #define MAX_STRING_SIZE 100
-// 3 2 1 3 4 1 6 2 4 3 4 2 1 4 5 2 1 3 4
-typedef struct
+
+typedef struct 
 {
-  int holding;
-  int frequency;
-  int duration;
+  int holdingPage;
+  int holdingDuration;
 } Frame;
 
-Frame *mainMemory;
+int frequencyArray[MAX_STRING_SIZE + 1];
+Frame mainMemory[MAX_STRING_SIZE];
 int hits = 0, misses = 0;
 int inputString[MAX_STRING_SIZE];
-int frequencyArray[MAX_STRING_SIZE + 1];
 int count = 0;
 
 void getInputString()
@@ -27,82 +26,86 @@ void getInputString()
 
 void initialize(int frameNo)
 {
-  mainMemory = (Frame*)malloc(frameNo * sizeof(Frame));
   for (int i = 0; i < frameNo; i++)
   {
-    mainMemory[i].holding = -1;
-    mainMemory[i].duration = 0;
-    mainMemory[i].frequency = 0;
+    mainMemory[i].holdingPage = -1;
+    mainMemory[i].holdingDuration = 0;
   }
   for (int i = 0; i < MAX_STRING_SIZE; i++)
-  {
     frequencyArray[i] = 0;
-  }
 }
 
-int checkPage(int frameNo, int nextPage)
+int checkIfExists(int frameNo, int nextPage)
 {
   for (int i = 0; i < frameNo; i++)
   {
-    if (mainMemory[i].holding == nextPage)
-      return i;
+    if (mainMemory[i].holdingPage == nextPage)
+      return 1;
   }
-  return INT_MIN;
+  return 0;
 }
 
-int leastFrequent(int frameNo, int newPage)
+int findReplacementPage(int frameNo)
 {
-  int min = 0;
+  int replacementIndex = 0;
   for (int i = 0; i < frameNo; i++)
   {
-    if (mainMemory[i].holding == -1)
-    {mainMemory[i].duration++;
+    if (mainMemory[i].holdingPage == -1)
       return i;
-    }
-    else if (frequencyArray[mainMemory[i].holding] < frequencyArray[mainMemory[min].holding]){
-      mainMemory[i].duration++;
-      min = i;
-    }
-    else if (frequencyArray[mainMemory[i].holding] == frequencyArray[mainMemory[min].holding])
+    else if (frequencyArray[mainMemory[i].holdingPage] < frequencyArray[mainMemory[replacementIndex].holdingPage])
+      replacementIndex = i;
+    else if (frequencyArray[mainMemory[i].holdingPage] == frequencyArray[mainMemory[replacementIndex].holdingPage])
     {
-      mainMemory[i].duration++;
-      if (mainMemory[i].duration > mainMemory[min].duration)
-        min = i;
-      else if (mainMemory[i].duration <= mainMemory[min].duration)
-        continue;
+      if (mainMemory[i].holdingDuration > mainMemory[replacementIndex].holdingDuration)
+        replacementIndex = i;
     }
   }
-  return min;
+  return replacementIndex;
 }
-void lfuPageReplacement(int frameNo)
+
+void updateDuration(int frameNo, int ind)
 {
-  int max;
+  for (int i = 0; i < frameNo; i++)
+  {
+    if (mainMemory[i].holdingPage != -1)
+      mainMemory[i].holdingDuration++;
+  }
+  if (ind == INT_MIN)
+  {
+    
+  }
+}
+
+void LFUPageReplacement(int frameNo)
+{
+  int res;
   for (int i = 0; i <= count; i++)
   {
-    if (checkPage(frameNo, inputString[i]) == INT_MIN)
+    if (checkIfExists(frameNo, inputString[i]) == 0)
     {
-      max = leastFrequent(frameNo, inputString[i]);
-      mainMemory[max].holding = inputString[i];
+      res = findReplacementPage(frameNo);
+      mainMemory[res].holdingPage = inputString[i];
       frequencyArray[inputString[i]]++;
+      updateDuration(frameNo, res);
       misses++;
     }
     else
     {
-      frequencyArray[inputString[i]]++;
       hits++;
+      updateDuration(frameNo);
     }
   }
 }
 
 int main()
 {
-  int frames;
-  printf("Enter an input string (values seperated by spaces) : ");
+  int frameNo;
+  printf("LFU PAGE REPLACEMENT ALGORITHM\n Enter an input string : ");
   getInputString();
   printf("Enter number of frames : ");
-  scanf("%d", &frames);
-  initialize(frames);
-  lfuPageReplacement(frames);
+  scanf("%d", &frameNo);
+  initialize(frameNo);
+  LFUPageReplacement(frameNo);
   printf("No of hits = %d\n", hits);
   printf("No of misses = %d\n", --misses);
   return 0;
